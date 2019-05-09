587f1a833805e4ce3daa75130649f5dfb6401642
derekgui <derek.guiz@gmail.com>
2019-05-09 20:03:59 +1200

    complete route model

--- src/route_model.cpp
+++ src/route_model.cpp
@@ -2,5 +2,49 @@
 #include <iostream>
 
 RouteModel::RouteModel(const std::vector<std::byte> &xml) : Model(xml) {
+    int counter = 0;
+    
+    for(auto node : Nodes()){
+        m_Nodes.push_back(Node(counter,this,node));
+        counter++;
+    }
+    CreateNodeToRoadHashMap();
 
+}
+
+void RouteModel::CreateNodeToRoadHashMap(void){
+    for(auto& road : Roads()){
+        if(road.type == Model::Road::Type::Footway)
+            continue;
+        for(int node_idx : Ways()[road.way].nodes){
+            if(node_to_road.find(node_idx)==node_to_road.end()){
+                node_to_road[node_idx] = std::vector<const Model::Road*>();
+            }
+            node_to_road[node_idx].push_back(&road);
+        }
+
+    }
+}
+
+RouteModel::Node* RouteModel::Node::FindNeighbor(std::vector<int> node_indices){
+        Node* closest_node = nullptr;
+        Node node;
+
+        for(int node_idx : node_indices){
+            node = parent_model->SNodes()[node_idx];
+            if(!node.visited && distance(node) != 0){
+                if(closest_node == nullptr || distance(node)< distance(*closest_node))
+                    closest_node = &parent_model->SNodes()[node_idx];
+            }
+        }
+        return closest_node;
+}
+
+void RouteModel::Node::FindNeighbors(void){
+        RouteModel::Node* new_neighbor = nullptr;
+        for(auto& road : parent_model->node_to_road[this->index]){
+            new_neighbor = FindNeighbor(parent_model->Ways()[road->way].nodes);
+            if(new_neighbor)
+                this->neighbors.push_back(new_neighbor);
+        }
 }
--- src/route_model.h
+++ src/route_model.h
@@ -12,21 +12,37 @@ class RouteModel : public Model {
     class Node : public Model::Node {
       public:
         // Add public Node variables and methods here.
-        
         Node(){}
         Node(int idx, RouteModel * search_model, Model::Node node) : Model::Node(node), parent_model(search_model), index(idx) {}
-      
+        Node* parent = nullptr;
+        float h_value = std::numeric_limits<float>::max();
+        float g_value = 0.0;
+        bool visited = false;
+        std::vector<Node *> neighbors;
+
+        float distance(Node n) const{
+            return std::sqrt(std::pow((x - n.x),2) + std::pow((y-n.y),2));
+            } 
+        void FindNeighbors(void);
+
       private:
         // Add private Node variables and methods here.
         int index;
         RouteModel * parent_model = nullptr;
+        Node* FindNeighbor(std::vector<int> node_indices);
     };
     
     // Add public RouteModel variables and methods here.
+    auto& SNodes(){return m_Nodes;}
+    auto& GetNodeToRoadMap() const{return node_to_road;}
     RouteModel(const std::vector<std::byte> &xml);  
     std::vector<Node> path; // This variable will eventually store the path that is found by the A* search.
 
   private:
     // Add private RouteModel variables and methods here.
+    std::unordered_map<int, std::vector<const Model::Road *>> node_to_road;
+    std::vector<Node> m_Nodes;
+    void CreateNodeToRoadHashMap(void);
+
 
 };
