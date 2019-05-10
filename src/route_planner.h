9305b1d6f942d192c6c8f5bcd356fdf8c0e7b883
derekgui <derek.guiz@gmail.com>
2019-05-10 13:51:13 +1200

    Add a constructFinalPath method to  iteratively traversing to the parent nodes  and store the sequence of the nodes

--- src/route_planner.h
+++ src/route_planner.h
@@ -10,9 +10,14 @@ class RoutePlanner {
   public:
     RoutePlanner(RouteModel &model, float start_x, float start_y, float end_x, float end_y);
     // Add public variables or methods declarations here.
-    
+    float GetDistance() const {return distance;}
+
 
   private:
     // Add private variables or methods declarations here.
+    std::vector<RouteModel::Node> ConstructFinalPath(RouteModel::Node* current_node); 
+    RouteModel::Node* start_node;
+    RouteModel::Node* end_node;
+    float distance;
     RouteModel &m_Model;
 };
